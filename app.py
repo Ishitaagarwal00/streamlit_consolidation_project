ab38d24d7cb4555fce55fc18980315ae3012575c
Enrique Rivero <enrique.rivero@gmail.com>
2025-02-16 11:52:10 +0100

    Added info section

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import streamlit.components.v1 as stc
 from eda_app import run_eda_app
 from ml_app import run_ml_app
 
-st.set_page_config(
-	page_title='Consolidation project: Streamlit',
-	layout='wide',
-	initial_sidebar_state='auto'
-)
+# st.set_page_config(
+# 	page_title='Consolidation project: Streamlit',
+# 	layout='wide',
+# 	initial_sidebar_state='auto'
+# )
 
 # Función main()
 def main():
@@ -32,7 +32,12 @@ def main():
 	elif menu == 'ML':
 		run_ml_app()
 	else:
-		st.markdown('Info ...')
+		st.subheader('Info')
+		st.markdown('Consolidation project using Streamlit library')
+		st.markdown('MBIT - Master in Data Engineering, Cloud & Big Data')
+		st.markdown('* * *')
+		st.markdown('###  A bit of info about diabetes mellitus')
+		stc.iframe("https://www.diabetes.co.uk/", width=900, height=500, scrolling=True)
 
 if __name__ == '__main__':
 	main()
